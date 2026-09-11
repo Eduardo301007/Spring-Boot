26cf3ae7ebeafd28354b51b3b85f93acf9efa8d2
Eduardo301007 <edu.ferreira3010@gmail.com>
2026-09-11 15:07:34 -0300

    Primeiras rotas básicas com controller

---
+++ first-springapp/src/main/java/com/teste/first_springapp/FirstSpringappApplication.java
@@ -0,0 +1,13 @@
+package com.teste.first_springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringappApplication.class, args);
+	}
+
+}
---
+++ first-springapp/src/test/java/com/teste/first_springapp/FirstSpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.first_springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
